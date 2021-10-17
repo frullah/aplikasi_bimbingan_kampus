cf1adfa4e876e3941953375db3b27d0fab98f942
Fajarullah <frullah12@gmail.com>
2021-10-17 09:22:13 +0700

    update

--- spec/requests/abouts_spec.rb
+++ spec/requests/abouts_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Abouts", type: :request do
   describe "GET /show" do
-    it "returns http success" do
-      get "/abouts/show"
-      expect(response).to have_http_status(:success)
+    fit "require authorization", aggregate_failures: true do
+      get "/about"
+      expect(response).to redirect_to(new_user_session_path)
+
+      sign_in create(:user)
+      get "/about"
+      expect(response).to have_http_status(:ok)
     end
   end
-
 end
